52805795d104f16df46061fdbaf8dd084cf4fed9
n.mekh <ecvr813b>
2020-10-29 11:16:59 +0300

    Bugfix: корректная передача номера заказа

--- src/ps_epos/ps_epos.php
+++ src/ps_epos/ps_epos.php
@@ -16,26 +16,6 @@ class Ps_Epos extends CmsgatePaymentModule
     public function __construct()
     {
         parent::__construct();
-//        $this->name = 'ps_epos';
-//        $this->tab = 'payments_gateways';
-//        $this->version = 'v1.1.8';
-//        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
-//        $this->author = 'esas';
-//        $this->bootstrap = true;
-//        $this->need_instance = 0; // а может все-таки 1?
-//        $this->is_eu_compatible = 1;
-//        parent::__construct();
-//
-//        $this->displayName = 'EPOS';
-//        $this->description = 'Super EPOS';
-//        $this->confirmUninstall = $this->trans('Are you sure about removing these details?', array(), 'Modules.Wirepayment.Admin'); //todo
-//
-//        foreach (Registry::getRegistry()->getConfigFormsArray() as $configForm) {
-//            foreach ($configForm->getManagedFields()->getFieldsToRender() as $configField) {
-//                if ($configField->isRequired() && empty($configField->getValue()))
-//                    $this->warning = $this->l('Field [' . $configField->getName() . '] is required.');
-//            }
-//        }
         $this->controllers = array('payment', 'paymentWebpay', 'callback');
     }
 
